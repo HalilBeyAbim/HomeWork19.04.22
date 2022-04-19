2172dd0dcfea4f27b75bbc6360489dacabcf4a0f
HalilBeyAbim <khalilbaygkh@code.edu.az>
2022-04-19 14:57:20 +0400

    added home work 19.02.22

---
+++ HomeWork19.04.22/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace HomeWork19._04._22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 10, 8, 49, 17, 5 };
+            Array.Sort(array);
+            int MaxValue = array[array.Length - 1];
+            Console.WriteLine(MaxValue);
+
+        }
+    }
+}
